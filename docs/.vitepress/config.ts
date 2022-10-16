998008166535dd86d47c0634faab55f76a02acb5
Loudomian <Loudomian@gmail.com>
2022-10-17 02:34:41 +0800

    ✨ Add umami tracker

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -21,7 +21,8 @@ export default defineConfig({
     ['meta', { property: 'og:url', content: ogUrl }],
     ['meta', { property: 'og:description', content: ogDescription }],
     ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
-    ['meta', { name: 'twitter:site', content: '@vite_js' }]
+    ['meta', { name: 'twitter:site', content: '@vite_js' }],
+    ['script', { src: 'https://umm.007.rip/umami.js', async: true, defer: true, 'data-website-id': 'd8d8bb05-56be-4f37-a4e0-64cc9a0c800a' }],
   ],
 
   lastUpdated: true,
